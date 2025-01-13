5b90270eac0f8bc1be037c312c24cd2482e84d48
max <max>
2025-01-13 10:56:06 +0100

    First commit, testing new IDE

---
+++ Store.Controllers/Controllers/ClientsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Store.Controllers.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ClientsController
+{
+    [HttpGet]
+    public IActionResult<IEnumerable<ClientResponseDto>> GetAllClients()
+    {
+        
+    }
+}
