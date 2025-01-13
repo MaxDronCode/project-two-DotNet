46fb9f6f9f4298f9cbaea53f4fda3a62665c39d8
max <max>
2025-01-13 12:21:01 +0100

    FEAT: Create client domain layer with client service interface and implementation. also client domain class

---
+++ Store.Domain/Models/ClientDomain.cs
@@ -0,0 +1,9 @@
+﻿namespace Store.Domain.Models;
+
+public class ClientDomain
+{
+    public string id { get; set; }
+    public string nif { get; set; }
+    public string name { get; set; }
+    public string address { get; set; }
+}
