01e5cd49cd031a4f9b8d97dacf8558fc0bf4f76c
max <max>
2025-01-13 12:21:45 +0100

    FEAT: Create mapper classes and functions in Domain and Controller layers

---
+++ Store.Controllers/Mappers/ClientMapper.cs
@@ -0,0 +1,20 @@
+﻿using Store.Controllers.Dtos;
+using Store.Domain.Models;
+
+namespace Store.Controllers.Mappers;
+
+public static class ClientMapper
+{
+    public static ClientResponseDto ToDto(this ClientDomain domain)
+    {
+        var dto = new ClientResponseDto
+        {
+            id = domain.id,
+            name = domain.name,
+            nif = domain.nif,
+            address = domain.address
+        };
+
+        return dto;
+    }
+}
---
+++ Store.Domain/Mappers/ClientMapper.cs
@@ -0,0 +1,20 @@
+﻿using Store.Domain.Models;
+using Store.Repository.Entities;
+
+namespace Store.Domain.Services.Mappers;
+
+public static class ClientMapper
+{
+    public static ClientDomain ToDomain(this ClientEntity entity)
+    {
+        var domain = new ClientDomain
+        {
+            id = entity.id,
+            name = entity.name,
+            nif = entity.nif,
+            address = entity.address
+        };
+
+        return domain;
+    }
+}
