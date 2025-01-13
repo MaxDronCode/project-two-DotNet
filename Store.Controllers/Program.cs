2c3b6eae1abacdecdc766b2cb6c43d7bd9f59848
max <max>
2025-01-13 12:23:24 +0100

    UPDATE: Update project dependencies structure in Program.cs and Store.Controllers.csproj

--- Store.Controllers/Program.cs
+++ Store.Controllers/Program.cs
@@ -1,3 +1,10 @@
+using Store.Repository.dbConfig;
+using Microsoft.EntityFrameworkCore;
+using Store.Domain.Services.Implementations;
+using Store.Domain.Services.Interfaces;
+using Store.Repository;
+using Store.Repository.Implementations;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +12,18 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+// MySql
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        new MySqlServerVersion(new Version(9, 1, 0)));
+});
+
+// Dependencies
+builder.Services.AddScoped<IClientRepository, ClientRepository>();
+builder.Services.AddScoped<IClientService, ClientService>();
 
 var app = builder.Build();
 
