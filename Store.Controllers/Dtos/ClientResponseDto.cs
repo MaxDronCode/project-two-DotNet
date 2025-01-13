2777aa7e1996a19ff90444ce0896fcfd70f86ebb
max <max>
2025-01-13 12:22:14 +0100

    FEAT: Create Client response dto Class

---
+++ Store.Controllers/Dtos/ClientResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Store.Controllers.Dtos;
+
+public class ClientResponseDto
+{
+    public string id { get; set; }
+    public string name { get; set; }
+    public string nif { get; set; }
+    public string address { get; set; }
+}
